fd972b8ce979ecb96819c35819720832207314cb
Paul Moore <wipmoore@outlook.com>
2017-04-13 13:10:02 +0100

    Robot Wars - 2hr mark

---
+++ RobotWars/RobotWars.Tests/RobotWarTests.cs
@@ -0,0 +1,166 @@
+﻿using Xunit;
+
+namespace RobotWars {
+
+    public class RobotWarTests
+    {
+
+        // done: can accept just an arena size
+
+        // done: robot rotates left from north to west        
+        // done: robot rotates left from west to south
+        // done: robot rotates left from south to east
+        // done: robot rotates left from east to north
+
+        // done: robot rotates right from north to east
+        // done: robot rotates right from east to south
+        // done: robot rotates right from south to west
+        // done: robot rotates right from west to north
+
+        // done: robot decrements X if oriented west when moving
+        // done: robot increments X if oriented east when moving
+        // done: robot decrements Y if oriented south when moving
+        // done: robot increments Y if oriented north when moving
+
+        // done: robot trying to move south of the arena results in no change of position
+        // done: robot trying to move north of the arena results in no change of position
+        // done: robot trying to move east of the arena results in no change of position 
+        // done: robot trying to move west of the arena results in no change of position
+
+        // done: robot wars accepts multiple robots
+
+        // Q. How should invalid input be handled?
+        //  - arena specification
+        //  - robot specification
+        //  - robot movement ( missing, malformed )
+
+        // N. Should probably use factories to separate the types from the command format.
+        //     Q. How would that work with the movement commands does the same logic apply?
+
+
+        [Fact]
+        public void can_accept_just_an_arena_size() {
+
+            Assert.Equal( string.Empty, robotWar.Play( "5 5" ) );
+
+        }
+
+
+        [Fact]
+        public void robot_rotates_left_from_north_to_west() {
+
+            Assert.Equal( "0 0 W", robotWar.Play( "5 5\n0 0 N\nL" ) );
+        }
+
+        [Fact]
+        public void robot_rotates_left_from_west_to_south() {
+
+            Assert.Equal( "0 0 S", robotWar.Play( "5 5\n0 0 W\nL" ) );
+
+        }
+
+        [Fact]
+        public void robot_rotates_left_from_south_to_east() {
+
+            Assert.Equal( "0 0 E", robotWar.Play( "5 5\n0 0 S\nL" ) );
+
+        }
+
+        [Fact]
+        public void robot_rotates_left_from_east_to_north() {
+
+            Assert.Equal( "0 0 N", robotWar.Play( "5 5\n0 0 E\nL" ) );
+        }
+
+
+        [Fact]
+        public void robot_rotates_right_from_north_to_east() {
+
+            Assert.Equal( "0 0 E", robotWar.Play( "5 5\n0 0 N\nR" ) );
+        }
+
+        [Fact]
+        public void robot_rotates_right_from_east_to_south() {
+
+            Assert.Equal( "0 0 S", robotWar.Play( "5 5\n0 0 E\nR" ) );
+
+        }
+
+        [Fact]
+        public void robot_rotates_right_from_south_to_west() {
+
+            Assert.Equal( "0 0 W", robotWar.Play( "5 5\n0 0 S\nR" ) );
+        }
+
+        [Fact]
+        public void robot_rotates_right_from_west_to_norht() {
+
+            Assert.Equal( "0 0 N", robotWar.Play( "5 5\n0 0 W\nR" ) );
+        }
+
+
+        [Fact]
+        public void robot_decrements_X_if_oriented_west_when_moving() {
+
+            Assert.Equal( "0 0 W", robotWar.Play( "5 5\n1 0 W\nM" ) );
+
+        }
+
+        [Fact]
+        public void robot_increments_X_if_oriented_east_when_moving() {
+
+            Assert.Equal( "1 0 E", robotWar.Play( "5 5\n0 0 E\nM" ) );
+        }
+
+        [Fact]
+        public void robot_decrements_Y_if_oriented_south_when_moving() {
+
+            Assert.Equal( "0 0 S", robotWar.Play( "5 5\n0 1 S\nM" ) );
+
+        }
+
+        [Fact]
+        public void robot_increments_Y_if_oriented_north_when_moving() {
+
+            Assert.Equal( "0 1 N", robotWar.Play( "5 5\n0 0 N\nM" ) );
+        }
+
+
+        [Fact]
+        public void robot_trying_to_move_south_of_the_arena_results_in_no_change_of_position() {
+
+            Assert.Equal( "0 0 S", robotWar.Play( "5 5\n0 0 S\nM" ) );
+
+        }
+
+        [Fact]
+        public void robot_trying_to_move_north_of_the_arena_results_in_no_change_of_position() {
+
+            Assert.Equal( "0 5 N", robotWar.Play( "5 5\n0 5 N\nM" ) );
+        }
+
+        [Fact]
+        public void robot_trying_to_move_east_of_the_arena_results_in_no_change_of_position() {
+
+            Assert.Equal( "5 0 E", robotWar.Play( "5 5\n5 0 E\nM" ) );
+
+        }
+
+        [Fact]
+        public void robot_trying_to_move_west_of_the_arena_results_in_no_change_of_position() {
+
+            Assert.Equal( "0 0 W", robotWar.Play( "5 5\n0 0 W\nM" ) );
+        }
+
+
+        [Fact]
+        public void robot_wars_accepts_multiple_robots() {
+
+            Assert.Equal( $"1 3 N\r\n5 1 E", robotWar.Play( "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM" ));
+        }
+
+        private RobotWar robotWar = new RobotWar();
+
+    }
+
+}
---
+++ RobotWars/RobotWars/RobotWar.cs
@@ -0,0 +1,176 @@
+﻿using System.Text;
+
+namespace RobotWars {
+
+    // Representation of a single robot war
+    public class RobotWar {
+
+        public string Play( string game ) {
+
+            var results = new StringBuilder();
+            var commands = game.Split( '\n' );
+            var arena = new Arena( commands[ 0 ] );
+
+            for( var i =1; i < commands.Length; i += 2 ) {
+                results.AppendLine( executeRobotMovements( arena, commands[ i ], commands[ i + 1 ] ) );
+            }
+
+            return results.ToString().Trim();
+        }
+
+        private string executeRobotMovements
+                        ( Arena arena
+                        , string startPosition
+                        , string movements ) {
+
+            var robot = new Robot( new Position( startPosition ), arena );
+                robot.Move( movements );
+
+            return robot.CurrentPosition.ToString();
+        }
+
+    }
+    
+    public class Robot {
+        private Arena arena;
+
+        public Robot
+                ( Position startPosition
+                , Arena arena ) {
+
+            this.CurrentPosition = startPosition;
+            this.arena = arena;
+        }
+
+        private const char Left  = 'L';
+        private const char Right = 'R';
+        private const char MovePosition = 'M';
+
+        public Position CurrentPosition { get; private set; }
+
+        public void Move( string movements ) {
+
+            foreach( var movement in movements ) {
+
+                switch( movement ) {
+                    case Left : CurrentPosition = CurrentPosition.RotateLeft(); break;
+                    case Right: CurrentPosition = CurrentPosition.RotateRight(); break;
+                    case MovePosition :
+                        var newPosition = CurrentPosition.Move();
+
+                        if( arena.IsWithinArena( newPosition ) ) {
+                            CurrentPosition = newPosition;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    public class Position {
+
+        public Position
+                ( string position ) {
+
+            var coordinates = position.Split( ' ' );
+            X = int.Parse( coordinates[ 0 ] );
+            Y = int.Parse( coordinates[ 1 ] );
+            Orientation = coordinates[ 2 ][ 0 ];
+
+        }
+
+        public Position
+                ( int x
+                , int y 
+                , char orientation ) {
+
+            X = x;
+            Y = y;
+            Orientation = orientation;
+        }
+
+        private const char North = 'N';
+        private const char East  = 'E';
+        private const char South = 'S';
+        private const char West  = 'W';
+
+        public int X { get; }
+        public int Y { get; }
+        public char Orientation { get; set; }
+
+        public override string ToString() {
+            return $"{X} {Y} {Orientation}";
+        }
+
+        public Position RotateLeft() {
+
+            var newOrientation = Orientation;
+            switch( Orientation ) {
+                case North : newOrientation = West; break;
+                case West  : newOrientation = South; break;
+                case South : newOrientation = East; break;
+                case East  : newOrientation = North; break;
+            }
+
+            return new Position( X, Y, newOrientation );
+        }
+
+        public Position RotateRight() {
+
+            var newOrientation = Orientation;
+            switch( Orientation ) {
+                case North : newOrientation = East; break;
+                case East  : newOrientation = South; break;
+                case South : newOrientation = West; break;
+                case West  : newOrientation = North; break;
+            }
+            return new Position( X, Y, newOrientation );
+        }
+
+        public Position Move() {
+            var newX = X + xMovementModifier();
+            var newY = Y + yMovementModifier();
+
+            return new Position( newX, newY, Orientation );
+        }
+
+        private int xMovementModifier() {
+            if( Orientation == West ) return -1;
+            if( Orientation == East ) return 1;
+
+            return 0;
+        }
+
+        private int yMovementModifier() {
+            if( Orientation == South ) return -1;
+            if( Orientation == North ) return 1;
+
+            return 0;
+        }
+
+    }
+
+    public class Arena {
+        private int width;
+        private int height;
+
+        public Arena
+                ( string dimentions ) {
+
+            var fact = dimentions.Split( ' ' );
+
+            width  = int.Parse( fact[ 0 ] );
+            height = int.Parse( fact[ 1 ] );
+        }
+
+        public bool IsWithinArena
+                     ( Position position ) {
+
+            if( position.X < 0 || position.X > width  ) return false;
+            if( position.Y < 0 || position.Y > height ) return false;
+
+            return true;
+        }
+
+    }
+}
