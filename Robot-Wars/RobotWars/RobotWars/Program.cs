888b09948ff40573f727fc4306118ac0ad2fdcba
Emil V. Toma <evtoma@gmail.com>
2019-07-25 08:00:43 +0100

    added code for final heading

--- Robot-Wars/RobotWars/RobotWars/Program.cs
+++ Robot-Wars/RobotWars/RobotWars/Program.cs
@@ -14,12 +14,9 @@ namespace RobotWars
             Console.WriteLine("             Welcome to the   R_O_B_O_T    W_A_R_S   Contest!!\n\n\n");
             Arena arena = new Arena();
             Robot robot1 = new Robot();
-            //robot1 =  WeveGotToMove();
+            robot1.SteerNMove();
                         
         }
-        public void WeveGotToMove()
-        {
-            //robot1.SteerNMove;
-        }
+        
     }
 }
--- Robot-Wars/RobotWars/RobotWars/Robot.cs
+++ Robot-Wars/RobotWars/RobotWars/Robot.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RobotWars
@@ -21,17 +22,89 @@ namespace RobotWars
         }
         public Robot()
         {
-            Console.WriteLine("\n\n Now enter your beautiful name, my star: \n");
+            Console.WriteLine("\nNow enter your beautiful name, my star: \n");
             string name = Console.ReadLine();
-            Console.WriteLine("\n\n\nThis is not for heart fainted! Enter the sequence that will steer and move your \nbuddy Robot to the desired place inside the Arena.\nThink (or Ram??) well " +name+ " darling, or you may not get to move the tin again :D\n");
-          
-            Console.ReadLine();
+            Console.WriteLine("\nThis is not for heart fainted! Enter the sequence that will steer and move your \nbuddy Robot to the desired place inside the Arena.\nThink (or Ram??) well, " +name+ " darling, or you may not get the chance to move the tin again :D\n");
+            
+
         }
         public void SteerNMove()
         {
             string rawMoveSequenceCapture = Console.ReadLine();
-            var rawSequence = rawMoveSequenceCapture.Split(' ');
+            int countLeftTurns = rawMoveSequenceCapture.Count(f => f == 'L');
+            int countRightTurns = rawMoveSequenceCapture.Count(f => f == 'R');
+            int countMoves = rawMoveSequenceCapture.Count(f => f == 'M');
+            int spinVector = countRightTurns - countLeftTurns;
+            char heading = '0';
+            //feedback, next three lines to be deleted
+            Console.WriteLine("Left " + countLeftTurns);
+            Console.WriteLine("Right " + countRightTurns);
+            Console.WriteLine("moves " + countMoves);
+            // computing final heading
+            //consider North position 0, East position 1, South position 2 and West position 3
+            //for clockwise steering, spinVector > 0
 
+            if (spinVector > 0)
+            {
+                if (spinVector % 4 == 0)
+                {
+                    heading = 'N';
+                }
+                else
+                {
+                    if (spinVector % 2 == 0)
+                    {
+                        heading = 'S';
+                    }
+                    else
+                    {
+                        if (spinVector % 3 == 0)
+                        {
+                            heading = 'W';
+                        }
+                        else
+                        {
+                            heading = 'E';
+                        }
+                    }
+                }
+            }
+
+            //for anti-clockwise steering spinVector < 0, the statement for East and West will reverse
+
+            if (spinVector < 0)
+            {
+                if (spinVector % 4 == 0)
+                {
+                    heading = 'N';
+                }
+                else
+                {
+                    if (spinVector % 2 == 0)
+                    {
+                        heading = 'S';
+                    }
+                    else
+                    {
+                        if (spinVector % 3 == 0)
+                        {
+                            heading = 'E';
+                        }
+                        else
+                        {
+                            heading = 'W';
+                        }
+                    }
+                }
+            }
+            //feedback from heading:
+            Console.WriteLine(heading);    
+            
+            //computing movement
+            
+            Console.ReadLine();
         }
+
+
     }
 }
